bcc3395e746dc8e50a6304b6e2558559cc53467a
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-05-24 23:14:21 +0700

    feat: add routes controller and service for chat

--- backend/routes/client/index.route.ts
+++ backend/routes/client/index.route.ts
@@ -1,11 +1,11 @@
 import { Application } from "express";
 import { userRoutes } from "./user.route";
 import { friendRoutes } from "./friend.route";
-
+import { chatRoutes } from "./chat.route";
 
 const clientRoutes = (app: Application)=>{
 app.use("/user", userRoutes)
 app.use("/friends", friendRoutes)
-
+app.use("/chat",chatRoutes)
 }
 export default clientRoutes;
